92af05188e8d262f9f976f5c04e73251a8212857
ugurkbc <ugur.kabacaoglu@outlook.com>
2022-05-23 21:43:31 +0300

    refactor imagewidget

--- imagewidget.cpp
+++ imagewidget.cpp
@@ -1,11 +1,15 @@
 #include "imagewidget.h"
 #include <QPainter>
 #include <QPaintEvent>
+#include <VideoCapture/videocapture.h>
 
-ImageWidget::ImageWidget(QWidget *parent)
+ImageWidget::ImageWidget(VideoCapture *pVideoCapture, QWidget *parent)
     : QWidget(parent)
+    , mVideoCapture(pVideoCapture)
 {
     mLocker = new QMutexLocker(&mMutex);
+
+    connect(mVideoCapture, &VideoCapture::onNewFrame, this, &ImageWidget::newImage, Qt::DirectConnection);
 }
 
 ImageWidget::~ImageWidget()
@@ -19,6 +23,8 @@ void ImageWidget::paintEvent(QPaintEvent *event)
     Q_UNUSED(event);
     QPainter lPainter(this);
     lPainter.drawImage((rect().bottomRight() - mImage.rect().bottomRight()) / 2, mImage);
+
+    emit onNewFrame(mImage);
 
     mLocker->unlock();
 }
--- imagewidget.h
+++ imagewidget.h
@@ -6,24 +6,29 @@
 #include <QMutexLocker>
 #include <QMutex>
 
+class VideoCapture;
 
 class ImageWidget : public QWidget
 {
     Q_OBJECT
 public:
-    explicit ImageWidget(QWidget *parent = nullptr);
+    explicit ImageWidget(VideoCapture *pVideoCapture, QWidget *parent = nullptr);
     ~ImageWidget();
 
 private:
     QImage mImage;
     QMutexLocker *mLocker;
     QMutex mMutex;
+    VideoCapture *mVideoCapture;
 
 protected:
     void paintEvent(QPaintEvent *event) override;
 
 public slots:
     void newImage(QImage pImage);
+
+signals:
+    void onNewFrame(QImage);
 };
 
 #endif // IMAGEWIDGET_H
