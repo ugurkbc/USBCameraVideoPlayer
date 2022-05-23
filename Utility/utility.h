6ecd349cf0d564919cb251fe481317ae1da7d34e
ugurkbc <ugur.kabacaoglu@outlook.com>
2022-05-23 21:38:53 +0300

    add utility

---
+++ Utility/utility.h
@@ -0,0 +1,42 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include <float.h>
+#include <math.h>
+
+class Utility
+{
+public:
+    static void toFraction(const double decimal, int &numerator_i, int &denominator_i)
+    {
+        double err = 1.0;
+        int denominator = 1;
+        double numerator = 0;
+        for (int check_denominator = 1; ; check_denominator++)
+        {
+            double check_numerator = (double)check_denominator * decimal;
+            double dummy;
+            double check_err = modf(check_numerator, &dummy);
+            if (check_err < err)
+            {
+                err = check_err;
+                denominator = check_denominator;
+                numerator = check_numerator;
+                if (err < FLT_EPSILON)
+                    break;
+            }
+            if (check_denominator == 100)  // limit
+                break;
+        }
+        numerator_i = std::round(numerator);
+        denominator_i = denominator;
+    }
+
+private:
+    Utility() {}
+public:
+    Utility(Utility const&)               = delete;
+    void operator=(Utility const&)  = delete;
+};
+
+#endif // UTILITY_H
